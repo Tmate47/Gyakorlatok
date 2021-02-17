86d3e9050fe8c68d46ad759ca165dcc413d8a9ec
Tmate47 <70635403+Tmate47@users.noreply.github.com>
2021-02-17 17:15:43 +0100

    Tesztaüzem
    Tesztaüzem

---
+++ Tesztauzem/tesztauzem.js
@@ -0,0 +1,9 @@
+﻿function Kiir(){
+    if(document.getElementById("jatek-valasz").value==""){
+        alert("Üres bejegyzés nem küldhető");
+    }
+    else{
+        alert("Köszönjük, hogy részt vesz a játékunkban!");
+        document.getElementById("jatek-valasz").value="";
+    }
+}
